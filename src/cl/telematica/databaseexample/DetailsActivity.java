0dea845487725f4453e4f3566c31e9f2a15c5822
Alonso Sandoval M. <a.sandoval.magnani@gmail.com>
2013-11-30 13:35:47 -0300

    Obteniendo datos de la tabla

--- src/cl/telematica/databaseexample/DetailsActivity.java
+++ src/cl/telematica/databaseexample/DetailsActivity.java
@@ -1,71 +1,76 @@
 package cl.telematica.databaseexample;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+//import cl.telematica.databaseexample.adapters.RssAdapter;
+import cl.telematica.databaseexample.adapters.TableAdapter;
+import cl.telematica.databaseexample.database.DataBaseClass;
+import cl.telematica.databaseexample.models.EarthQuakeDataModel;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Bitmap;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+//import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+//import android.view.View;
+//import android.webkit.WebChromeClient;
+//import android.webkit.WebView;
+//import android.webkit.WebViewClient;
+//import android.widget.ArrayAdapter;
+import android.widget.ListView;
+//import android.widget.ProgressBar;
+//import android.widget.RelativeLayout;
 
 public class DetailsActivity extends Activity {
+	private DataBaseClass dbInstance;
+	//private String url = null;
 	
-	private String url = null;
-	private WebView webView;
-	private ProgressBar mProgressBar;
-	private RelativeLayout hPBarLayout;
+	private List<EarthQuakeDataModel> list = new ArrayList<EarthQuakeDataModel>();
+	private ListView lstDetails;
 	
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_detail);
 		
-		webView = (WebView) findViewById(R.id.webView);
-		hPBarLayout = (RelativeLayout) findViewById(R.id.hPBarLayout);
-		mProgressBar = (ProgressBar) findViewById(R.id.legacy_navigation_progressBar);
+		dbInstance = new DataBaseClass(this);
 		
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.getSettings().setBuiltInZoomControls(true);
+		getFromDataBase();
+	}
+	
+	//private void getFromDataBase(String[] lst){
+	private void getFromDataBase(){
+		SQLiteDatabase db = dbInstance.getReadableDatabase();
+		Cursor c = db.rawQuery("SELECT * FROM alumnos LIMIT 7", null);
 		
-		webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress)
-            {
-                mProgressBar.setProgress(progress);
-            }
-        });
-  
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
-            {
-                // Handle the error
-            }
-  
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
-            {
-                view.loadUrl(url);
-                return true;
-            }
-            
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon){
-            	hPBarLayout.setVisibility(View.VISIBLE);
-            	super.onPageStarted(view, url, favicon);
-            }
-            
-            @Override
-            public void onPageFinished(WebView view, String url){
-            	hPBarLayout.setVisibility(View.GONE);
-            	super.onPageFinished(view, url);
-            }
-        });
+
+		if(c.moveToFirst()){
+			do{
+			   EarthQuakeDataModel model = new EarthQuakeDataModel();
+			   model.title = c.getString(1);
+			   model.magnitude = c.getString(2);
+			   model.location = c.getString(3);
+			   model.depth = c.getString(4);
+			   model.latitude = c.getString(5);
+			   model.longitude = c.getString(6);
+			   model.dateTime = c.getString(7);
+			   model.link = c.getString(8);
+			   list.add(model);
+			} while (c.moveToNext());
+		}
+		c.close();
 		
-		webView.loadUrl(url);
+		/*ArrayAdapter<String> adaptador =
+			    new ArrayAdapter<String>(this,
+			        android.R.layout.simple_list_item_1, lst);*/
+		TableAdapter adapter = new TableAdapter(getApplicationContext(), R.string.app_name, list);
+		
+		lstDetails = (ListView)findViewById(R.id.LstDetails);
+		 
+		lstDetails.setAdapter(adapter);
 	}
 
 }
